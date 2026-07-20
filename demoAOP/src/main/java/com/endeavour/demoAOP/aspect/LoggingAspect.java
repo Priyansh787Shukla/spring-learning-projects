ab6cc97bc6b2714931d669453140e93c5425f200
Priyansh787Shukla <priyanshshukla675@gmail.com>
2026-07-20 23:19:48 +0530

    Added the Point-Cut-Expression in the Logging Aspect

--- demoAOP/src/main/java/com/endeavour/demoAOP/aspect/LoggingAspect.java
+++ demoAOP/src/main/java/com/endeavour/demoAOP/aspect/LoggingAspect.java
@@ -1,12 +1,14 @@
 package com.endeavour.demoAOP.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoggingAspect
 {
+    @Before("execution(String com.endeavour.demoAOP.service.StudentService.create())")
     public void log()
     {
         System.out.println("Student is going to be saved");
