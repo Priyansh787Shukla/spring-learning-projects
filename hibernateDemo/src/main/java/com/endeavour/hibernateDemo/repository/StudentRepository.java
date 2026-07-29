1e9cc05f08b6d190828d408074590b1529ea0967
Priyansh787Shukla <priyanshshukla675@gmail.com>
2026-07-29 18:47:47 +0530

    mentioned the lifecycle of Entity->Transient, Managed, Detached, Removed. Methods: persist(), find(), flush(), detach(), clear(), merge(), remove()

--- hibernateDemo/src/main/java/com/endeavour/hibernateDemo/repository/StudentRepository.java
+++ hibernateDemo/src/main/java/com/endeavour/hibernateDemo/repository/StudentRepository.java
@@ -18,7 +18,13 @@ public class StudentRepository
 
     public Student get(long id)
     {
-        return entityManager.find(Student.class, id);
+        Student s1 = entityManager.find(Student.class, id);
+        //entityManager.detach(s1); //detaches s1 from persistence context
+        //entityManager.merge(); - re-attaches s1 to persistence context
+        //entityManager.clear(); - detaches all objects from persistence context
+        //entityManager.flush(); - syncs the DB with Persistence context
+        //entityManager.remove(s1); - deleted s1 from DB
+        return s1;
     }
 
     public void delete(Student st)
