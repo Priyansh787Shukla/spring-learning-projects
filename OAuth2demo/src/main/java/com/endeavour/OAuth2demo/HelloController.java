031c9ae10bb1c11fcd05df73664c5f45f01eaa23
Priyansh787Shukla <priyanshshukla675@gmail.com>
2026-08-01 19:26:31 +0530

    OAuth2 Login practiced using Google and GitHub

--- OAuth2demo/src/main/java/com/endeavour/OAuth2demo/HelloController.java
+++ OAuth2demo/src/main/java/com/endeavour/OAuth2demo/HelloController.java
@@ -1,4 +1,14 @@
 package com.endeavour.OAuth2demo;
 
-public class HelloCOntroller {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController
+{
+    @GetMapping("/")
+    public String greet()
+    {
+        return "Hello World";
+    }
 }
--- OAuth2demo/src/main/java/com/endeavour/OAuth2demo/SecurityConfig.java
+++ OAuth2demo/src/main/java/com/endeavour/OAuth2demo/SecurityConfig.java
@@ -1,4 +1,21 @@
 package com.endeavour.OAuth2demo;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig
+{
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+    {
+        http.authorizeHttpRequests(auth->auth.anyRequest().authenticated());
+        http.oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
 }
